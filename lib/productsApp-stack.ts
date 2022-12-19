d044eafd25e76fdea308264773c007f29b5c08a6
Jonas Gabriel <jonas@wedsys.com.br>
2022-12-19 12:07:14 -0300

    primeira funcao lambda criada

---
+++ lib/productsApp-stack.ts
@@ -0,0 +1,24 @@
+import * as lambda from 'aws-cdk-lib/aws-lambda'
+import * as lambdaNodeJs from 'aws-cdk-lib/aws-lambda-nodejs'
+import * as cdk from 'aws-cdk-lib'
+import { Construct } from 'constructs'
+
+export class ProductsAppStack extends cdk.Stack {
+  readonly productsFetchHandler: lambdaNodeJs.NodejsFunction
+
+  constructor (scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props)
+
+    this.productsFetchHandler = new lambdaNodeJs.NodejsFunction(this, 'ProductsFetchFunction', {
+      functionName: 'ProductsFetchFunction',
+      entry: 'lambda/products/productsFetchFunction.ts',
+      handler: 'handler',
+      memorySize: 128,
+      timeout: cdk.Duration.seconds(5),
+      bundling: {
+        minify: true,
+        sourceMap: false
+      }
+    })
+  }
+}
