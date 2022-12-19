c7c3d49bd78950108740d1586d7a660f21af4771
Jonas Gabriel <jonas@wedsys.com.br>
2022-12-19 11:44:34 -0300

    primeiro commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# cloud-aws
-Repositorio criado com objetivo de adquirir experiencia com serviços da AWS
+# AWS-CLOUD
+
+Projeto criado para adquirir experiencias com serviços da AWS
+
+## Useful commands
+
+* `npm run build`   compile typescript to js
+* `npm run watch`   watch for changes and compile
+* `npm run test`    perform the jest unit tests
+* `cdk deploy`      deploy this stack to your default AWS account/region
+* `cdk diff`        compare deployed stack with current state
+* `cdk synth`       emits the synthesized CloudFormation template
---
+++ test/cloud-aws.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CloudAws from '../lib/cloud-aws-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cloud-aws-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CloudAws.CloudAwsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
