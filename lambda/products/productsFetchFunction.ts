b70920f080fe7e2f7bbd99e42804e1da55e10bf8
Jonas Gabriel <jonas@wedsys.com.br>
2022-12-19 12:16:04 -0300

    capturando requestIDs

--- lambda/products/productsFetchFunction.ts
+++ lambda/products/productsFetchFunction.ts
@@ -2,11 +2,16 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda
 
 export async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
 
-  const method = event.httpMethod
+  const lambdaRequestId = context.awsRequestId
+  const apiGatewayId =event.requestContext.requestId
 
+  console.log(`API Gateway requestId: ${apiGatewayId} - Lambda requestId: ${lambdaRequestId}`)
+
+  const method = event.httpMethod
   if (event.resource === '/products') {
     if (method === 'GET') {
       console.log('GET')
+      
       return {
         statusCode: 200,
         body: JSON.stringify({
