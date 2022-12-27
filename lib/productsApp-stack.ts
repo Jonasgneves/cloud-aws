895e3f896c8783aaa62bfe77de1e05fb347ae823
Jonas Gabriel <jonas@wedsys.com.br>
2022-12-27 11:15:33 -0300

    implementando lambda insights

--- lib/productsApp-stack.ts
+++ lib/productsApp-stack.ts
@@ -45,7 +45,8 @@ export class ProductsAppStack extends cdk.Stack {
         PRODUCTS_DDB: this.productsDdb.tableName
       },
       layers: [productsLayer],
-      tracing: lambda.Tracing.ACTIVE
+      tracing: lambda.Tracing.ACTIVE,
+      insightsVersion: lambda.LambdaInsightsVersion.VERSION_1_0_119_0
     })
     this.productsDdb.grantReadData(this.productsFetchHandler)
 
@@ -65,7 +66,8 @@ export class ProductsAppStack extends cdk.Stack {
         PRODUCTS_DDB: this.productsDdb.tableName
       },
       layers: [productsLayer],
-      tracing: lambda.Tracing.ACTIVE
+      tracing: lambda.Tracing.ACTIVE,
+      insightsVersion: lambda.LambdaInsightsVersion.VERSION_1_0_119_0
     })
     this.productsDdb.grantWriteData(this.productsAdminHandler)
 
