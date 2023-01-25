1a3b27d4a1af6ae71ea931f7bf5aff839fe6a6b0
Jonas <jonas_gn@hotmail.com>
2023-01-25 09:42:58 -0300

    criacao de CRUD de orderRepository

--- lambda/orders/layers/ordersLayer/nodejs/orderRepository.ts
+++ lambda/orders/layers/ordersLayer/nodejs/orderRepository.ts
@@ -1,3 +1,6 @@
+import { DocumentClient } from "aws-sdk/clients/dynamodb"
+import { v4 as uuid } from 'uuid'
+ 
 export interface OrderProduct {
   code: string,
   price: number,
@@ -16,4 +19,74 @@ export interface Order {
     totalPrice: number
   },
   products: OrderProduct[]
+}
+
+export class OrderRepository {
+  private ddbClient: DocumentClient
+  private orderDdb: string
+
+  constructor (ddbClient: DocumentClient, orderDdb: string) {
+    this.ddbClient = ddbClient
+    this.orderDdb = orderDdb
+  }
+
+  async createOrder(order: Order): Promise<Order> {
+    order.sk = uuid()
+    order.createdAt = Date.now()
+    await this.ddbClient.put({
+      TableName: this.orderDdb,
+      Item: order
+    }).promise()
+    return order
+  }
+
+  async getAllOrders(): Promise<Order[]> {
+    const data = await this.ddbClient.scan({
+      TableName: this.orderDdb
+    }).promise()
+    return data.Items as Order[]
+  }
+   
+  async getOrderByEmail(email: string): Promise<Order[]> {
+    const data = await this.ddbClient.query({
+      TableName: this.orderDdb,
+      KeyConditionExpression: 'pk = :email',
+      ExpressionAttributeValues: {
+        ':email': email
+      }
+    }).promise()
+    return data.Items as Order[]
+  }
+
+  async getOrder(email: string, orderId: string): Promise<Order> {
+    const data = await this.ddbClient.get({
+      TableName: this.orderDdb,
+      Key: {
+        pk: email,
+        sk: orderId
+      }
+    }).promise()
+    if (data.Item) {
+      return data.Item as Order
+    } else {
+      throw new Error('Order not found')
+    }
+  }
+
+  async deletOrder(email: string, orderId: string): Promise<Order> {
+    const data = await this.ddbClient.delete({
+      TableName: this.orderDdb,
+      Key: {
+        pk: email,
+        sk: orderId
+      },
+      ReturnValues: 'ALL_OLD'
+    }).promise()
+    if (data.Attributes) {
+      return data.Attributes as Order
+    } else {
+      throw new Error('Order not found')
+    }
+  }
+  
 }
